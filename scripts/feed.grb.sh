43652acdaafe479ba7ab85787699da213c9ef872
Glenn Plas <glenn@byte-consult.be>
2016-11-12 18:53:20 +0100

    import data

--- scripts/feed.grb.sh
+++ scripts/feed.grb.sh
@@ -8,3 +8,22 @@ echo "Fetch GRB data ..."
 cd /usr/local/src/
 
 wget --quiet $DATA
+
+# CREATE EXTENSION postgis;
+# CREATE EXTENSION postgis_topology;
+# CREATE EXTENSION hstore;
+echo "Enable PostGIS on GRB database ..."
+
+psql --username=postgres --dbname=grb-api -c "CREATE EXTENSION postgis;"
+psql --username=postgres --dbname=grb-api -c "CREATE EXTENSION postgis_topology;"
+psql --username=postgres --dbname=grb-api -c "CREATE EXTENSION hstore;"
+
+# psql --set ON_ERROR_STOP=on dbname < infile
+
+echo "Importing data ..."
+zcat /usr/local/src/grb.gz | psql psql --set ON_ERROR_STOP=on grb-api
+
+echo "Done"
+
+sleep 2
+
