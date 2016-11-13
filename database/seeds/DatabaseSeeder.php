7f593a0124cd2bfd7e52576c45a59178423c4e59
Glenn Plas <glenn@byte-consult.be>
2016-11-13 02:34:51 +0100

    Adding api

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,16 +1,13 @@
 <?php
 
-use Illuminate\Database\Seeder;
+use App\Port\Seeder\Abstracts\Seeder;
 
+/**
+ * Class DatabaseSeeder
+ *
+ * @author  Mahmoud Zalt  <mahmoud@zalt.me>
+ */
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        // $this->call(UsersTableSeeder::class);
-    }
+
 }
